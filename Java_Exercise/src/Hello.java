69e9c666feebd6c3527476922b80873018caeb32
pp-8 <vishakha.p@prominentpixel.com>
2023-08-23 11:36:13 +0530

    Added new files SecondClass and Hello in Java_Exercise project

---
+++ Java_Exercise/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Vishakha");
+        System.out.println("Good Morning");
+        System.out.print("How was your day?");
+    }
+}
