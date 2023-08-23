b3e82632d5d682b6f396adbb06de194b81a5c9f2
pp-8 <vishakha.p@prominentpixel.com>
2023-08-23 12:06:25 +0530

    Added IfThen.java file in Java_Exercise Project

---
+++ Java_Exercise/src/IfThen.java
@@ -0,0 +1,11 @@
+public class IfThen {
+    public static void main(String[] args) {
+        System.out.println("Good Morning!");
+
+        boolean isAlien = true;
+        if(isAlien == true) {
+            System.out.println("It is an Alien.");
+            System.out.println("Ans I am scared of aliens.");
+        }
+    }
+}
