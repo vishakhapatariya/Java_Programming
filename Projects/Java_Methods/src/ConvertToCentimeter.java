6f19ba090ad1044573b53d93814842f5792e03a6
pp-8 <vishakha.p@prominentpixel.com>
2023-08-24 15:18:28 +0530

    Added two files MethodOverloading.java and It's exercise ConvertToCentimeter.java in Java_Methods Project

---
+++ Projects/Java_Methods/src/ConvertToCentimeter.java
@@ -0,0 +1,14 @@
+public class ConvertToCentimeter {
+    public static void main(String[] args){
+        System.out.println("60inch is "+convertToCentimeters(68)+" Centimeters");
+        System.out.println("5ft 8inch is "+convertToCentimeters(5,8)+" Centimeters");
+    }
+    public static double convertToCentimeters(int inch){
+        // 1 inch = 2.54 centimeters
+        return inch*2.54;
+    }
+    public static double convertToCentimeters(int foot , int inch){
+        // 1 foot = 12 inch
+        return convertToCentimeters((foot*12)+inch);
+    }
+}
