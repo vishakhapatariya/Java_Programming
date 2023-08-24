bac98e66861445ea963c09e6f67810d32dc1c963
pp-8 <vishakha.p@prominentpixel.com>
2023-08-24 12:52:13 +0530

    Added new file PositiveNegativeZero.java in Java_Methods Project

--- Projects/Java_Methods/src/MethodChallenge.java
+++ Projects/Java_Methods/src/MethodChallenge.java
@@ -1,29 +1,29 @@
 public class MethodChallenge {
 
-    public static void displayHighScorePosition(String name,int score){
+    public static void displayHighScorePosition(String name, int score) {
         int position = calculateHighScorePosition(score);
         System.out.println(name + " managed to get into position " + position + " on the high score list.");
     }
 
-    public static int calculateHighScorePosition(int score){
-        int position=4;
+    public static int calculateHighScorePosition(int score) {
+        int position = 4;
 
-        if(score>=1000){
+        if (score >= 1000) {
             position = 1;
-        } else if (score>=500) {
-            position=2;
-        } else if (score>=100) {
-            position=3;
+        } else if (score >= 500) {
+            position = 2;
+        } else if (score >= 100) {
+            position = 3;
         }
 
         return position;
     }
 
     public static void main(String[] args) {
-        displayHighScorePosition("Vishakha",1500);
-        displayHighScorePosition("Amika",1000);
-        displayHighScorePosition("Sakshi",500);
-        displayHighScorePosition("Nilam",100);
-        displayHighScorePosition("Dhruvi",25);
+        displayHighScorePosition("Vishakha", 1500);
+        displayHighScorePosition("Amika", 1000);
+        displayHighScorePosition("Sakshi", 500);
+        displayHighScorePosition("Nilam", 100);
+        displayHighScorePosition("Dhruvi", 25);
     }
 }
