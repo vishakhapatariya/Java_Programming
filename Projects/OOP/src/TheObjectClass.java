4e80c1d626f6ceb98b5bdbea4b742d2b68460f14
pp-8 <vishakha.p@prominentpixel.com>
2023-08-29 14:32:44 +0530

    java.lang.object Exercise

---
+++ Projects/OOP/src/TheObjectClass.java
@@ -0,0 +1,49 @@
+// java.lang.object Exercise
+
+public class TheObjectClass extends Object {
+    public static void main(String[] args) {
+        Students max = new Students("max",21);
+        System.out.println(max);
+
+        PrimaryScoolStudent jimmy = new PrimaryScoolStudent("Jimmy",8,"Carol");
+        System.out.println(jimmy);
+    }
+}
+
+class Students {
+    private String name;
+    private int age;
+
+    Students(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+//    @Override
+//    public String toString() {
+//        return super.toString();
+//    }
+
+
+    @Override
+    public String toString() {
+        return name + " is "+ age;
+//        return "Students{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+    }
+}
+
+class PrimaryScoolStudent extends Students{
+    private String parentName;
+    PrimaryScoolStudent(String name,int age,String parentName){
+        super(name,age);
+        this.parentName=parentName;
+    }
+
+    @Override
+    public String toString() {
+        return parentName + "'s kid, " +super.toString();
+    }
+}
