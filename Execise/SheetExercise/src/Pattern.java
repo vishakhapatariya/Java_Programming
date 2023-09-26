61e61c51bd31fe5fee05611fe48682f2466688fe
pp-8 <vishakha.p@prominentpixel.com>
2023-09-26 12:34:52 +0530

    Pattern exercise of sheet

---
+++ Execise/SheetExercise/src/Pattern.java
@@ -0,0 +1,47 @@
+/* 1) Write a program to generate below output. N=9. Put validation like user input should be odd number. If user entered even number then program should display proper message to the user.
+        *           * * * * *
+        *         * * * *
+        *       * * *
+        *    * *
+        *  *
+        *    * *
+        *       * * *
+        *         * * * *
+        *          * * * * *  */
+
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String args[]) {
+        int n = 0;
+        Scanner sc = new Scanner(System.in);
+        while (n % 2 == 0) {
+            System.out.print("Enter Odd Integer : ");
+            n = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (j == 0) {
+                    System.out.print("*");
+                }
+
+                if (i <= n / 2) {
+                    if (j >= (n / 2) - i) {
+                        System.out.print("*".repeat((n / 2) - i + 1));
+                        break;
+                    } else {
+                        System.out.print(" ");
+                    }
+                } else {
+                    if (j < i - (n / 2)) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*".repeat(i - (n / 2) + 1));
+                        break;
+                    }
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
