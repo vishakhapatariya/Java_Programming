ffc5d485373d85439dd7e797f2301847dcf55e97
pp-8 <vishakha.p@prominentpixel.com>
2023-09-27 17:00:57 +0530

    Float, Character and Boolean Wrapper classes

---
+++ Projects/JavaLangPackage/src/BooleanWrapper.java
@@ -0,0 +1,11 @@
+// Boolean class methods
+
+public class BooleanWrapper {
+    public static void main(String[] args) {
+        Boolean a = true;
+        Boolean b = false;
+
+        // compare() method
+        System.out.println(Boolean.compare(a, b));
+    }
+}
