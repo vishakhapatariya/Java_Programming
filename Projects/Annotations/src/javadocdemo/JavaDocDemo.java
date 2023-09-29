94f7ede20f658c673e03689602988a134750cc0e
pp-8 <vishakha.p@prominentpixel.com>
2023-09-29 12:18:25 +0530

    Created Java Documentation

---
+++ Projects/Annotations/src/javadocdemo/JavaDocDemo.java
@@ -0,0 +1,15 @@
+// Java Documentation Demo
+
+/**
+ * @author vishakha
+ */
+package javadocdemo;
+
+/**
+ * Class Having Main Method
+ */
+public class JavaDocDemo {
+    public static void main(String[] args) {
+
+    }
+}
